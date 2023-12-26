00d91efbfde27e3992475579a455251355af48d0
Soporte <soporte@menaresycia.com>
2023-12-26 11:23:30 -0300

    añadir cambiar correos

---
+++ dags/configurations/cambiar_correos.py
@@ -0,0 +1,38 @@
+import warnings
+import logging
+from airflow.models import DAG
+from airflow.utils.dates import timedelta, days_ago
+from airflow.providers.microsoft.mssql.operators.mssql import MsSqlOperator
+
+
+default_args = {
+    "owner": "adrian",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "email": ["soporte@menaresycia.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
+    "catchup_by_default": False,
+    "execution_timeout": timedelta(seconds=1800),
+}
+
+
+with DAG(
+    "cambiar_correos_webcob",
+    default_args=default_args,
+    description="cambia el correo utilizado en webcob.",
+    schedule_interval=None,
+    max_active_runs=1,
+    concurrency=4,
+    tags=["informes", "sql server"],
+) as dag:
+    
+    create_hab_table = MsSqlOperator(
+        task_id="ejecutar_script_cambio_correos",
+        mssql_conn_id="mssql_menares_34",
+        sql="queries/cambia_correos_webcob.sql",
+        split_statements=True,
+        autocommit=True,
+        dag=dag
+    )
---
+++ dags/configurations/queries/cambia_correos_webcob.sql
@@ -0,0 +1,50 @@
+use webcob
+declare @ema_actual varchar(200)
+declare @pas_actual varchar(200)
+declare @ema_nuevo varchar(200)
+declare @pas_nuevo varchar(200)
+
+/*login*/
+select * from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPL' 
+/*contraseña*/
+select * from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPP'
+
+select @ema_actual = val_texto
+      ,@ema_nuevo  = val_texto2 
+from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPL'
+
+
+select @pas_actual = val_texto
+      ,@pas_nuevo  = val_texto2 
+from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPP'
+
+select @ema_actual  ema_actual
+	  ,@pas_actual	pas_actual
+	  ,@ema_nuevo 	ema_nuevo 
+	  ,@pas_nuevo 	pas_nuevo 
+
+/*Actualiza*/
+
+UPDATE ut_cob_parametros_m 
+set val_texto = @ema_nuevo
+,   val_texto2 = @ema_actual
+where cod_app='SMAIL' and cod_parametro='SMTPL'
+
+
+UPDATE ut_cob_parametros_m 
+set val_texto  = @pas_nuevo
+,   val_texto2 = @pas_actual
+where cod_app='SMAIL' and cod_parametro='SMTPP'
+
+
+
+/*login*/
+select * from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPL' 
+/*contraseña*/
+select * from ut_cob_parametros_m where cod_app='SMAIL' and cod_parametro='SMTPP'
+
+
+	  /*
+	  informacionprevisional@menaresycia.com	menaresIP	contactoafp@menaresycia.com	menaresAFP
+	  
+	  */
